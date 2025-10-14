6922a34deb7e0713b0ef83aea3e86dc278761aaf
Ro-11Omega <omegar196@gmail.com>
2025-10-14 06:28:30 -0600

    Update

--- MatrizHill/Cifrado.py
+++ MatrizHill/Cifrado.py
@@ -1,8 +1,33 @@
 
 
 class Cifrado:
-    def __init__(self, textoClaro):
-        self.textoClaro = textoClaro
+    def __init__(self):
+        pass
 
 
+    def conversion(self, textoClaro, n):
+        numeros = []
+        for c in textoClaro:
+            if c == " ":
+                numeros.append(26)
+            else:
+                numeros.append(ord(c) - ord('A'))
 
+        print("Valores numéricos:", numeros)
+        print(f"Longitud original: {len(numeros)}")
+        
+        if len(numeros) % n != 0:
+            padding_needed = n - (len(numeros) % n)
+            numeros.extend([27] * padding_needed)
+            print(f"Se añadieron {padding_needed} caracteres de padding (27)")
+        
+        print(f"Longitud después del padding: {len(numeros)}")
+        print("Valores numéricos con padding:", numeros)
+
+        return numeros
+
+    def div_bloques(self, numeros, bloques, n):
+        for i in range(0, len(numeros), n):
+            bloque = numeros[i:i+n]
+            bloques.append(bloque)
+        return bloques
--- MatrizHill/Main.py
+++ MatrizHill/Main.py
@@ -2,11 +2,16 @@ from Cifrado import Cifrado
 from Descifrado import Descifrado
 from Key import Key
 
+cifrado = Cifrado()
+
 def menu():
     print("Cifrado: Matriz de Hill")
     text = input("Ingresa el mensaje a cifrar: ")
     textoClaro = text.upper()
-    
+    numeros = []
+    bloques = []
+
+
     while True:
         try:
             n = int(input("Ingresa el tamaño de bloque n (≥ 2): "))
@@ -20,33 +25,14 @@ def menu():
     print(f"Mensaje a cifrar: {textoClaro}")
     print(f"Usando bloques de tamaño: {n}")
 
-    numeros = []
     
-    for c in textoClaro:
-        if c == " ":
-            numeros.append(26)
-        else:
-            numeros.append(ord(c) - ord('A'))
+    numeros = cifrado.conversion(textoClaro,n)
+    bloques = cifrado.div_bloques(numeros, bloques, n)
 
-    print("Valores numéricos:", numeros)
-    print(f"Longitud original: {len(numeros)}")
-    
-    if len(numeros) % n != 0:
-        padding_needed = n - (len(numeros) % n)
-        numeros.extend([27] * padding_needed)
-        print(f"Se añadieron {padding_needed} caracteres de padding (27)")
-    
-    print(f"Longitud después del padding: {len(numeros)}")
-    print("Valores numéricos con padding:", numeros)
-
-    bloques = []
-    for i in range(0, len(numeros), n):
-        bloque = numeros[i:i+n]
-        bloques.append(bloque)
-    
     print(f"\n{len(bloques)} bloques de tamaño {n}:")
     for i, bloque in enumerate(bloques):
         print(f"Bloque {i+1}: {bloque}")
+
 
 if __name__ == "__main__":
     menu()
